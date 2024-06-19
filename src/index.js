cbb35b23ccddb1a2e8cdd1ec771b118d103d7954
wnnow <117201813+wnnow@users.noreply.github.com>
2024-06-19 18:21:30 +0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import './style.css';
+import printMe from './modules/print.js';
+import img from './images/small-fluffy-dog-breeds-maltipoo-66300ad363389.jpg';
+
+function component() {
+  const element = document.createElement('div');
+  const btn = document.createElement('button');
+  // Lodash, now imported by this script
+  element.textContent = 'Hello webpack';
+  element.classList.add('hello');
+
+  const myImg = new Image();
+  myImg.src = img;
+  btn.textContent = 'Click me and check the console!';
+  btn.onclick = printMe;
+
+  element.appendChild(btn);
+
+  element.appendChild(myImg);
+
+  return element;
+}
+
+document.body.appendChild(component());
